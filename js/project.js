009cc30b97ff3ec9b3de739fb5095bf79287c0d8
RaulPastranaCobo <rapasco97@gmail.com>
2025-01-23 16:17:21 +0100

    Formulario

--- js/project.js
+++ js/project.js
@@ -32,5 +32,41 @@ function scrollPercentage(){
 }
 
 window.onscroll=function(){
+    if(document.documentElement.scrollTop>=200){
+        toTop.style.display='block'
+    
+    }else{
+        toTop.style.display='none'
+    }
     scrollPercentage()
 }
+
+const toTop=document.getElementById('toTop')
+
+function getToTop(){
+    window.scrollTo({top:0,behavior:'smooth' })
+}
+
+toTop.addEventListener('click',getToTop)
+
+
+const formulario=document.getElementById('formulario')
+
+formulario.addEventListener('submit',evento =>{
+    evento.preventDefault()
+    const nombre=document.getElementById('nameInputId').value
+    const email=document.getElementById('emailInputId').value
+    const datos={
+        nombre:nombre,
+        email:email
+    }
+
+    fetch("https://jsonplaceholder.typicode.com/posts/1/comments", {
+        method:'POST',
+        body:JSON.stringify(datos)
+    })
+    .then(respuesta=>respuesta.json())
+    
+        
+    
+})
